25926f1c21f9c5b82ae3bbc1ff2bfafc3af71b50
vamolessa <1696072+vamolessa@users.noreply.github.com>
2021-11-08 22:55:43 -0300

    funciona mostrar os resultados

--- main.js
+++ main.js
@@ -1,15 +1,20 @@
 const SUBMIT_BUTTON_VALUE = "Buscar";
 
 const STATE = {
+	database: null,
+
 	cpfInputElement: null,
 	submitInputElement: null,
-	database: null,
+	resultsElement: null,
+	resultTemplate: null,
 };
 
 downloadDatabase();
 window.onload = function() {
 	STATE.cpfInputElement = document.getElementById("cpf-input");
 	STATE.submitInputElement = document.getElementById("submit-input");
+	STATE.resultsElement = document.querySelector(".results");
+	STATE.resultTemplate = document.getElementById("result-template");
 
 	if (STATE.database == null) {
 		STATE.submitInputElement.value = "Atualizando Banco de Dados...";
@@ -25,13 +30,36 @@ function onFormSubmit() {
 		return;
 	}
 
-	const query = STATE.cpfInputElement.textContent;
-	searchDatabase(query);
+	const query = STATE.cpfInputElement.value;
+	console.log("query: ", query);
+
+	while (STATE.resultsElement.lastChild) {
+		STATE.resultsElement.removeChild(STATE.resultsElement.lastChild);
+	}
+
+	for (const entry of STATE.database) {
+		if (entry.cpf != query) {
+			continue;
+		}
+
+		console.log("add entry: ", entry);
+		const entryElement = createElementFromEntry(entry);
+		STATE.resultsElement.appendChild(entryElement);
+	}
+
 	return false;
 }
 
-function searchDatabase(query) {
-	alert("buscando por ", query);
+function createElementFromEntry(entry) {
+	const element = STATE.resultTemplate.content.cloneNode(true);
+
+	const cpfElement = element.querySelector(".cpf");
+	cpfElement.textContent = entry.cpf;
+
+	const nameElement = element.querySelector(".name");
+	nameElement.textContent = entry.name;
+
+	return element;
 }
 
 function downloadDatabase() {
@@ -41,10 +69,10 @@ function downloadDatabase() {
 	request.onreadystatechange = function() {
 		if (request.readyState === request.DONE) {
 			if (request.status === 200) {
-				STATE.database = request.responseText;
-				console.log(STATE.database.length);
+				STATE.database = parseDatabase(request.responseText);
+				console.log("entry count: ", STATE.database.length);
 			} else {
-				STATE.database = "";
+				STATE.database = [];
 			}
 
 			STATE.submitInputElement.disabled = false;
@@ -54,3 +82,19 @@ function downloadDatabase() {
 	request.send();
 }
 
+function parseDatabase(text) {
+	return [
+		{
+			cpf: "1",
+			name: "alberto",
+		},
+		{
+			cpf: "2",
+			name: "bernado",
+		},
+		{
+			cpf: "3",
+			name: "carlitos",
+		}
+	];
+}
