67515761e9e263b2d16643c09bbcc4d9e0dafdb2
vamolessa <1696072+vamolessa@users.noreply.github.com>
2021-11-08 21:36:50 -0300

    fazendo as requisições http

--- main.js
+++ main.js
@@ -1,1 +1,17 @@
+const STATE = {
+	cpfInputElement: null,
+};
 
+window.onload = function() {
+	STATE.cpfInputElement = document.getElementById("cpf-input");
+};
+
+function onFormSubmit() {
+	const query = STATE.cpfInputElement.textContent;
+	searchDatabase(query);
+	return false;
+}
+
+function searchDatabase(query) {
+	alert("buscando por ", query);
+}
