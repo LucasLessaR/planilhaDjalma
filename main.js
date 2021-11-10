4969f40b6b4eeba2d5b580ee9d57f0f413d1c8fc
vamolessa <1696072+vamolessa@users.noreply.github.com>
2021-11-09 22:06:34 -0300

    funciona!!

--- main.js
+++ main.js
@@ -6,6 +6,8 @@ const STATE = {
 	cpfInputElement: null,
 	submitInputElement: null,
 	resultsBodyElement: null,
+	resultsInvoiceTotalElement: null,
+	resultsPaidTotalElement: null,
 	resultTemplate: null,
 };
 
@@ -14,6 +16,8 @@ window.onload = function() {
 	STATE.cpfInputElement = document.getElementById("cpf-input");
 	STATE.submitInputElement = document.getElementById("submit-input");
 	STATE.resultsBodyElement = document.querySelector(".results tbody");
+	STATE.resultsInvoiceTotalElement = document.querySelector(".results tfoot .invoice");
+	STATE.resultsPaidTotalElement = document.querySelector(".results tfoot .paid");
 	STATE.resultTemplate = document.getElementById("result-template");
 
 	if (STATE.databaseLines == null) {
@@ -31,22 +35,34 @@ function onFormSubmit() {
 	}
 
 	const query = STATE.cpfInputElement.value;
-	console.log("query: ", query);
 
 	while (STATE.resultsBodyElement.lastChild != null) {
 		STATE.resultsBodyElement.removeChild(STATE.resultsBodyElement.lastChild);
 	}
+
+	let invoiceTotal = 0.0;
+	let paidTotal = 0.0;
 
 	for (const entry of STATE.databaseLines) {
 		if (entry.cpf != query) {
 			continue;
 		}
 
+		invoiceTotal += entry.invoice;
+		paidTotal += entry.paid;
+
 		const entryElement = createElementFromEntry(entry);
 		STATE.resultsBodyElement.appendChild(entryElement);
 	}
 
+	STATE.resultsInvoiceTotalElement.textContent = invoiceTotal;
+	STATE.resultsPaidTotalElement.textContent = paidTotal;
+
 	return false;
+}
+
+function formatCurrency(n) {
+	return n.toLocaleString(undefined, {minimumFractionDigits: 2});
 }
 
 function createElementFromEntry(entry) {
@@ -56,10 +72,10 @@ function createElementFromEntry(entry) {
 	timestampElement.textContent = entry.timestamp;
 
 	const invoiceElement = element.querySelector(".invoice");
-	invoiceElement.textContent = entry.invoice;
+	invoiceElement.textContent = formatCurrency(entry.invoice);
 
 	const paidElement = element.querySelector(".paid");
-	paidElement.textContent = entry.paid;
+	paidElement.textContent = formatCurrency(entry.paid);
 
 	return element;
 }
@@ -84,28 +100,47 @@ function downloadDatabase() {
 	request.send();
 }
 
+function parseCurrency(text) {
+	return parseFloat(text.replace(",", "."));
+}
+
 function parseDatabase(text) {
 	const lines = text.split(/\r\n|\n\r|\n|\r/);
-	console.log(lines.length);
+	lines.shift();
 
-	return [
-		{
-			cpf: "123",
-			timestamp: "hoje",
-			invoice: "34",
-			paid: "35",
-		},
-		{
-			cpf: "123",
-			timestamp: "ontem",
-			invoice: "34",
-			paid: "35",
-		},
-		{
-			cpf: "123",
-			timestamp: "amanha",
-			invoice: "34",
-			paid: "35",
+	const entries = [];
+
+	const COLUMN_COUNT = 4;
+	for (const line of lines) {
+		if (line.length == 0) {
+			continue;
 		}
-	];
+	
+		const parts = line.split(";", COLUMN_COUNT);
+		if (parts.length < COLUMN_COUNT) {
+			console.error("menos de", COLUMN_COUNT, "colunas:\n", line);
+			continue;
+		}
+
+		const invoice = parseCurrency(parts[2]);
+		if (invoice == NaN) {
+			console.error("nao deu pra extrair numero a partir do 'faturado':\n", line);
+			continue;
+		}
+
+		const paid = parseCurrency(parts[3]);
+		if (paid == NaN) {
+			console.error("nao deu pra extrair numero a partir do 'pago':\n", line);
+			continue;
+		}
+
+		entries.push({
+			cpf: parts[0],
+			timestamp: parts[1],
+			invoice: invoice,
+			paid: paid,
+		});
+	}
+
+	return entries;
 }
